07417424746dc9e3317de93969e34ddfbb1cb5e9
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-04-22 23:31:22 +0200

    Add spring boot

---
+++ src/test/java/game/pandemic/PandemicApplicationTests.java
@@ -0,0 +1,13 @@
+package game.pandemic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandemicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
