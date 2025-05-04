1c81c39a362799ec1be418cffb751d907c65dc0e
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-05-04 16:22:28 +0200

    Add oauth2 support

--- src/main/java/game/pandemic/config/WebSecurityConfig.java
+++ src/main/java/game/pandemic/config/WebSecurityConfig.java
@@ -1,8 +1,10 @@
 package game.pandemic.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManagerResolver;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,10 +14,18 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
+    private final AuthenticationManagerResolver<HttpServletRequest> authenticationManagerResolver;
+
     @Bean
     public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
         return http
                 .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .authenticationManagerResolver(this.authenticationManagerResolver)
+                )
                 .build();
     }
 }
