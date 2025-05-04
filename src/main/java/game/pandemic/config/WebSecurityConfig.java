48ce144bc9e62feba04c6fed3628688a7a22432b
Tom <tom.weelborg@gmail.com>
2025-05-04 15:30:52 +0200

    Add cors config

---
+++ src/main/java/game/pandemic/config/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package game.pandemic.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
+        return http
+                .cors(Customizer.withDefaults())
+                .build();
+    }
+}
