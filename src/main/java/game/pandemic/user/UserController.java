2f11cf6c35d75afedf79986cccc2c461bf3c6d76
Tom <tom.weelborg@gmail.com>
2025-06-23 17:48:41 +0200

    Add validation

--- src/main/java/game/pandemic/user/UserController.java
+++ src/main/java/game/pandemic/user/UserController.java
@@ -1,10 +1,12 @@
 package game.pandemic.user;
 
 import game.pandemic.auth.Account;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,10 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(UserController.REQUEST_MAPPING)
 @RequiredArgsConstructor
+@Validated
 public class UserController {
     public static final String REQUEST_MAPPING = "/user";
     public static final String GET_ACCESS_TOKEN_URL = "/access-token";
     public static final String GET_ACCESS_TOKEN_OAUTH2_URL = GET_ACCESS_TOKEN_URL + "/oauth2";
+
+    public static final String BLANK_USERNAME_VALIDATION_EXCEPTION_MESSAGE = "Username may not be blank";
 
     public final UserService userService;
 
@@ -28,7 +33,7 @@ public class UserController {
     }
 
     @GetMapping(GET_ACCESS_TOKEN_URL)
-    public ResponseEntity<String> getAccessTokenForGuestUser(@RequestParam final String username) {
+    public ResponseEntity<String> getAccessTokenForGuestUser(@RequestParam @NotBlank(message = BLANK_USERNAME_VALIDATION_EXCEPTION_MESSAGE) final String username) {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(this.userService.getAccessTokenForGuestUser(username));
