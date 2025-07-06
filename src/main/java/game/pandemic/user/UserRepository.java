cbd528b0778f3f1aafb8cffbe31eb70619abf196
Tom <tom.weelborg@gmail.com>
2025-07-06 20:49:20 +0200

    Add User WebSocket

--- src/main/java/game/pandemic/user/UserRepository.java
+++ src/main/java/game/pandemic/user/UserRepository.java
@@ -1,6 +1,7 @@
 package game.pandemic.user;
 
 import game.pandemic.auth.Account;
+import game.pandemic.websocket.auth.IWebSocketAuthenticationObjectRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +9,10 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, IWebSocketAuthenticationObjectRepository<User> {
     Optional<User> findUserByAccount(final Account account);
     Optional<User> findUserByAccessToken(final UUID accessToken);
+    default Optional<User> findByAccessToken(final UUID accessToken) {
+        return this.findUserByAccessToken(accessToken);
+    }
 }
---
+++ src/main/java/game/pandemic/user/websocket/UserWebSocketHandler.java
@@ -0,0 +1,34 @@
+package game.pandemic.user.websocket;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import game.pandemic.user.User;
+import game.pandemic.websocket.WebSocketSessionRegistry;
+import game.pandemic.websocket.auth.IWebSocketAuthenticationObjectRepository;
+import game.pandemic.websocket.endpoint.IWebSocketController;
+import game.pandemic.websocket.endpoint.WebSocketHandler;
+import jakarta.validation.Validator;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class UserWebSocketHandler extends WebSocketHandler<User> {
+    public UserWebSocketHandler(final WebSocketSessionRegistry<User> webSocketSessionRegistry,
+                                final IWebSocketAuthenticationObjectRepository<User> webSocketAuthenticationObjectRepository,
+                                final List<IWebSocketController<User>> webSocketControllers,
+                                final ObjectMapper objectMapper,
+                                final Validator validator) {
+        super(
+                webSocketSessionRegistry,
+                webSocketAuthenticationObjectRepository,
+                webSocketControllers,
+                objectMapper,
+                validator
+        );
+    }
+
+    @Override
+    public String getEndpointMapping() {
+        return "/user";
+    }
+}
