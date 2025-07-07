9c0272fdf63a38f781d1b2206d7ed21bf832f9a3
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-07-07 02:25:38 +0200

    Add IJsonIdentityInfo

---
+++ src/main/java/game/pandemic/jackson/IJsonIdentityInfo.java
@@ -0,0 +1,11 @@
+package game.pandemic.jackson;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@JsonIdentityInfo(
+        generator = ObjectIdGenerators.PropertyGenerator.class,
+        property = "id"
+)
+public interface IJsonIdentityInfo {
+}
