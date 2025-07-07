4413ad18ee47c910ac1c031336488aaf9236db38
Tom <tom.weelborg@gmail.com>
2025-07-07 02:25:38 +0200

    Add IJsonIdentityInfo

--- src/main/java/game/pandemic/websocket/IWebSocketData.java
+++ src/main/java/game/pandemic/websocket/IWebSocketData.java
@@ -1,6 +1,7 @@
 package game.pandemic.websocket;
 
+import game.pandemic.jackson.IJsonIdentityInfo;
 import game.pandemic.jackson.IJsonTypeInfo;
 
-public interface IWebSocketData extends IJsonTypeInfo {
+public interface IWebSocketData extends IJsonIdentityInfo, IJsonTypeInfo {
 }
