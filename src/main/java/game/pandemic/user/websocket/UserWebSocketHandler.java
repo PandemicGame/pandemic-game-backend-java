dd8fd0b79c6b2fcf730e8af3b0dcf2a1c701209b
Tom <tom.weelborg@gmail.com>
2025-07-08 02:19:17 +0200

    Add WebSocket authentication success response

--- src/main/java/game/pandemic/user/websocket/UserWebSocketHandler.java
+++ src/main/java/game/pandemic/user/websocket/UserWebSocketHandler.java
@@ -1,6 +1,7 @@
 package game.pandemic.user.websocket;
 
 import game.pandemic.jackson.ObjectMapper;
+import game.pandemic.messaging.messengers.IUnicastMessenger;
 import game.pandemic.user.User;
 import game.pandemic.websocket.WebSocketSessionRegistry;
 import game.pandemic.websocket.auth.IWebSocketAuthenticationObjectRepository;
@@ -8,18 +9,21 @@ import game.pandemic.websocket.endpoint.IWebSocketController;
 import game.pandemic.websocket.endpoint.WebSocketHandler;
 import jakarta.validation.Validator;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketSession;
 
 import java.util.List;
 
 @Component
 public class UserWebSocketHandler extends WebSocketHandler<User> {
     public UserWebSocketHandler(final WebSocketSessionRegistry<User> webSocketSessionRegistry,
+                                final IUnicastMessenger<WebSocketSession> webSocketSessionUnicastMessenger,
                                 final IWebSocketAuthenticationObjectRepository<User> webSocketAuthenticationObjectRepository,
                                 final List<IWebSocketController<User>> webSocketControllers,
                                 final ObjectMapper objectMapper,
                                 final Validator validator) {
         super(
                 webSocketSessionRegistry,
+                webSocketSessionUnicastMessenger,
                 webSocketAuthenticationObjectRepository,
                 webSocketControllers,
                 objectMapper,
