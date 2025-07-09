35d6674d2649f6bf7a5c86b197f6ef98511d6947
Tom <tom.weelborg@gmail.com>
2025-07-10 01:18:18 +0200

    Update messenger delegators

--- src/main/java/game/pandemic/messaging/delegators/IGeneralPurposeMessengerDelegator.java
+++ src/main/java/game/pandemic/messaging/delegators/IGeneralPurposeMessengerDelegator.java
@@ -1,6 +1,24 @@
 package game.pandemic.messaging.delegators;
 
+import game.pandemic.messaging.messengers.IBroadcastMessenger;
 import game.pandemic.messaging.messengers.IGeneralPurposeMessenger;
+import game.pandemic.messaging.messengers.IUnicastAndMulticastMessenger;
 
-public interface IGeneralPurposeMessengerDelegator<T> extends IGeneralPurposeMessenger<T>, IUnicastMessengerDelegator<T>, IMulticastMessengerDelegator<T>, IBroadcastMessengerDelegator<T> {
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public interface IGeneralPurposeMessengerDelegator<T> extends IGeneralPurposeMessenger<T>, IUnicastAndMulticastMessengerDelegator<T>, IBroadcastMessengerDelegator<T> {
+    Map<Class<? extends T>, IGeneralPurposeMessenger<? extends T>> getGeneralPurposeMessengersForType();
+
+    @Override
+    default Map<Class<? extends T>, IUnicastAndMulticastMessenger<? extends T>> getUnicastAndMulticastMessengersForTypes() {
+        return new HashMap<>(getGeneralPurposeMessengersForType());
+    }
+
+    @Override
+    default Set<IBroadcastMessenger<? extends T>> getBroadcastMessengers() {
+        return new HashSet<>(getGeneralPurposeMessengersForType().values());
+    }
 }
--- src/main/java/game/pandemic/messaging/delegators/IUnicastAndMulticastMessengerDelegator.java
+++ src/main/java/game/pandemic/messaging/delegators/IUnicastAndMulticastMessengerDelegator.java
@@ -1,6 +1,22 @@
 package game.pandemic.messaging.delegators;
 
+import game.pandemic.messaging.messengers.IMulticastMessenger;
 import game.pandemic.messaging.messengers.IUnicastAndMulticastMessenger;
+import game.pandemic.messaging.messengers.IUnicastMessenger;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public interface IUnicastAndMulticastMessengerDelegator<T> extends IUnicastAndMulticastMessenger<T>, IUnicastMessengerDelegator<T>, IMulticastMessengerDelegator<T> {
+    Map<Class<? extends T>, IUnicastAndMulticastMessenger<? extends T>> getUnicastAndMulticastMessengersForTypes();
+
+    @Override
+    default Map<Class<? extends T>, IUnicastMessenger<? extends T>> getUnicastMessengersForTypes() {
+        return new HashMap<>(getUnicastAndMulticastMessengersForTypes());
+    }
+
+    @Override
+    default Map<Class<? extends T>, IMulticastMessenger<? extends T>> getMulticastMessengersForTypes() {
+        return new HashMap<>(getUnicastAndMulticastMessengersForTypes());
+    }
 }
