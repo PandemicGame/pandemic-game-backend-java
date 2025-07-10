dc97c253dbe1db32c03447b5a598e288d95b2dac
Tom <tom.weelborg@gmail.com>
2025-07-10 02:45:31 +0200

    Add lobby leaving

--- src/main/java/game/pandemic/lobby/websocket/UserLobbyMemberWebSocketHandler.java
+++ src/main/java/game/pandemic/lobby/websocket/UserLobbyMemberWebSocketHandler.java
@@ -1,6 +1,7 @@
 package game.pandemic.lobby.websocket;
 
 import game.pandemic.jackson.ObjectMapper;
+import game.pandemic.lobby.LobbyService;
 import game.pandemic.lobby.member.UserLobbyMember;
 import game.pandemic.messaging.messengers.IUnicastAndMulticastMessenger;
 import game.pandemic.validation.ValidationService;
@@ -8,19 +9,24 @@ import game.pandemic.websocket.WebSocketSessionRegistry;
 import game.pandemic.websocket.auth.IWebSocketAuthenticationObjectRepository;
 import game.pandemic.websocket.endpoint.IWebSocketController;
 import game.pandemic.websocket.endpoint.WebSocketHandler;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.WebSocketSession;
 
 import java.util.List;
 
 @Component
 public class UserLobbyMemberWebSocketHandler extends WebSocketHandler<UserLobbyMember> {
+    private final LobbyService lobbyService;
+
     public UserLobbyMemberWebSocketHandler(final WebSocketSessionRegistry<UserLobbyMember> webSocketSessionRegistry,
                                            final IUnicastAndMulticastMessenger<WebSocketSession> webSocketSessionMessenger,
                                            final IWebSocketAuthenticationObjectRepository<UserLobbyMember> webSocketAuthenticationObjectRepository,
                                            final List<IWebSocketController<UserLobbyMember>> iWebSocketControllers,
                                            final ObjectMapper objectMapper,
-                                           final ValidationService validationService) {
+                                           final ValidationService validationService,
+                                           final LobbyService lobbyService) {
         super(
                 webSocketSessionRegistry,
                 webSocketSessionMessenger,
@@ -29,10 +35,17 @@ public class UserLobbyMemberWebSocketHandler extends WebSocketHandler<UserLobbyM
                 objectMapper,
                 validationService
         );
+        this.lobbyService = lobbyService;
     }
 
     @Override
     public String getEndpointMapping() {
         return "/lobby";
+    }
+
+    @Override
+    public void afterConnectionClosed(@NonNull final WebSocketSession session, @NonNull final CloseStatus status) {
+        this.webSocketSessionRegistry.findAuthenticationObjectForSession(session).ifPresent(this.lobbyService::leaveLobby);
+        super.afterConnectionClosed(session, status);
     }
 }
