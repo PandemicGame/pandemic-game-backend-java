d9b87642149de02d0c27af1b30230029afb24b8c
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-07-15 20:42:54 +0200

    Add game WebSocket support

---
+++ src/main/java/game/pandemic/game/GameService.java
@@ -0,0 +1,9 @@
+package game.pandemic.game;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class GameService {
+}
