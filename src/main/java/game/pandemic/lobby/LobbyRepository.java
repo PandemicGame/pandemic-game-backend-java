926a5bd7724f893bfbbb9e7e748f044e5c0476c9
Tom <tom.weelborg@gmail.com>
2025-07-15 20:21:52 +0200

    Add lobby closing to stopping application

--- src/main/java/game/pandemic/lobby/LobbyRepository.java
+++ src/main/java/game/pandemic/lobby/LobbyRepository.java
@@ -1,7 +1,10 @@
 package game.pandemic.lobby;
 
 import game.pandemic.lobby.member.LobbyMember;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +15,8 @@ public interface LobbyRepository extends JpaRepository<Lobby, Long> {
     Optional<Lobby> findLobbyByMembersContaining(final LobbyMember lobbyMember);
     Optional<Lobby> findByIdAndIsClosedFalse(final Long id);
     List<Lobby> findAllByIsClosedFalse();
+    @Modifying
+    @Transactional
+    @Query("UPDATE Lobby l SET l.isClosed = true")
+    int closeAll();
 }
