5686bdb3afe6536e93c4157f6f103921e3d55533
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-07-17 02:48:45 +0200

    Add roles

---
+++ src/main/java/game/pandemic/game/role/Role.java
@@ -0,0 +1,30 @@
+package game.pandemic.game.role;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import game.pandemic.jackson.JacksonView;
+import game.pandemic.util.Color;
+import game.pandemic.websocket.IWebSocketData;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Role implements IWebSocketData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonView(JacksonView.Read.class)
+    private Long id;
+    @JsonView(JacksonView.Read.class)
+    private String name;
+    @Embedded
+    @JsonView(JacksonView.Read.class)
+    private Color color;
+
+    public Role(final String name, final Color color) {
+        this.name = name;
+        this.color = color;
+    }
+}
--- src/main/java/game/pandemic/lobby/events/StartGameLobbyEvent.java
+++ src/main/java/game/pandemic/lobby/events/StartGameLobbyEvent.java
@@ -2,12 +2,16 @@ package game.pandemic.lobby.events;
 
 import game.pandemic.game.Game;
 import game.pandemic.game.board.type.BoardType;
+import game.pandemic.game.role.Role;
 import game.pandemic.lobby.Lobby;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -15,9 +19,11 @@ import lombok.NoArgsConstructor;
 public class StartGameLobbyEvent extends LobbyEvent {
     @ManyToOne
     private BoardType boardType;
+    @ManyToMany
+    private List<Role> roles;
 
     @Override
     public void apply(final Lobby lobby) {
-        new Game(lobby, this.boardType);
+        new Game(lobby, this.boardType, this.roles);
     }
 }
