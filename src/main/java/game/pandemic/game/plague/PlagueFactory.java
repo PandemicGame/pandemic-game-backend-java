71820f0d5d12b87e245f07d8cc3fb2ee110e85eb
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-07-17 02:00:29 +0200

    Generalize PlagueColor to Color

--- src/main/java/game/pandemic/game/plague/PlagueFactory.java
+++ src/main/java/game/pandemic/game/plague/PlagueFactory.java
@@ -1,5 +1,6 @@
 package game.pandemic.game.plague;
 
+import game.pandemic.util.Color;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -8,10 +9,10 @@ import java.util.List;
 public class PlagueFactory {
     public List<Plague> createAllPlagues() {
         return List.of(
-                new Plague(PlagueCode.BLUE.getCode(), "Blue", new PlagueColor(0, 0, 255)),
-                new Plague(PlagueCode.YELLOW.getCode(), "Yellow", new PlagueColor(255, 255, 0)),
-                new Plague(PlagueCode.BLACK.getCode(), "Black", new PlagueColor(128, 128, 128)),
-                new Plague(PlagueCode.RED.getCode(), "Red", new PlagueColor(255, 0, 0))
+                new Plague(PlagueCode.BLUE.getCode(), "Blue", new Color(0, 0, 255)),
+                new Plague(PlagueCode.YELLOW.getCode(), "Yellow", new Color(255, 255, 0)),
+                new Plague(PlagueCode.BLACK.getCode(), "Black", new Color(128, 128, 128)),
+                new Plague(PlagueCode.RED.getCode(), "Red", new Color(255, 0, 0))
         );
     }
 }
