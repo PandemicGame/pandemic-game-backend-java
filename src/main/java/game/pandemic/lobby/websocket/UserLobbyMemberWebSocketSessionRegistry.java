cf6151354e6a6d86d0dbb5661999c72fa5a3f452
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-08-10 23:54:35 +0200

    Replace repository call in WebSocketSessionRegistry with saving attribute on session

--- src/main/java/game/pandemic/lobby/websocket/UserLobbyMemberWebSocketSessionRegistry.java
+++ src/main/java/game/pandemic/lobby/websocket/UserLobbyMemberWebSocketSessionRegistry.java
@@ -2,12 +2,8 @@ package game.pandemic.lobby.websocket;
 
 import game.pandemic.lobby.member.UserLobbyMember;
 import game.pandemic.websocket.WebSocketSessionRegistry;
-import game.pandemic.websocket.auth.IWebSocketAuthenticationObjectRepository;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserLobbyMemberWebSocketSessionRegistry extends WebSocketSessionRegistry<UserLobbyMember> {
-    public UserLobbyMemberWebSocketSessionRegistry(final IWebSocketAuthenticationObjectRepository<UserLobbyMember> webSocketAuthenticationObjectRepository) {
-        super(webSocketAuthenticationObjectRepository);
-    }
 }
