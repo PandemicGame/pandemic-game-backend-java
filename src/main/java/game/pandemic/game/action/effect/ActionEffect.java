357bb3ff4412663268c2b64e9e45b0160d85deb6
Tom <tom.weelborg@gmail.com>
2025-08-11 00:48:02 +0200

    Add Action

---
+++ src/main/java/game/pandemic/game/action/effect/ActionEffect.java
@@ -0,0 +1,26 @@
+package game.pandemic.game.action.effect;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import game.pandemic.game.player.Player;
+import game.pandemic.jackson.JacksonView;
+import game.pandemic.websocket.IWebSocketData;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public abstract class ActionEffect implements IWebSocketData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonView(JacksonView.Read.class)
+    private Long id;
+    @ManyToOne
+    protected Player executingPlayer;
+
+    protected ActionEffect(final Player executingPlayer) {
+        this.executingPlayer = executingPlayer;
+    }
+
+    public abstract void execute();
+}
