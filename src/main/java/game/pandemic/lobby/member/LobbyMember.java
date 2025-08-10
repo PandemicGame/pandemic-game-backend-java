51ef36af3ee1b734346a04ee20f27ac37e0c9ee4
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-08-11 01:55:49 +0200

    Fix lobby fetching issues in LobbyMember

--- src/main/java/game/pandemic/lobby/member/LobbyMember.java
+++ src/main/java/game/pandemic/lobby/member/LobbyMember.java
@@ -6,6 +6,7 @@ import game.pandemic.chat.ChatMessageSender;
 import game.pandemic.jackson.JacksonView;
 import game.pandemic.lobby.Lobby;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
@@ -17,7 +18,7 @@ import lombok.Setter;
 @EqualsAndHashCode(callSuper = true, onlyExplicitlyIncluded = true)
 public abstract class LobbyMember extends ChatMessageSender {
     @Setter
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JsonView(JacksonView.Read.class)
     @JsonIdentityReference(alwaysAsId = true)
     protected Lobby lobby;
