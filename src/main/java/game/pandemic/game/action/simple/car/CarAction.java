4f4dfc38b9e8584a4ee451d2745bfceebd578bf9
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-08-11 02:26:43 +0200

    Add CarAction

---
+++ src/main/java/game/pandemic/game/action/simple/car/CarAction.java
@@ -0,0 +1,25 @@
+package game.pandemic.game.action.simple.car;
+
+import game.pandemic.game.action.effect.MoveActionEffect;
+import game.pandemic.game.action.simple.MoveAction;
+import game.pandemic.game.player.Player;
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CarAction extends MoveAction {
+    public CarAction(final Player executingPlayer) {
+        super(executingPlayer);
+    }
+
+    @Override
+    protected List<MoveActionEffect> createAvailableMoveEffects() {
+        return this.executingPlayer.getCurrentField().getConnections().stream()
+                .map(field -> new MoveActionEffect(this.executingPlayer, this.executingPlayer, field))
+                .toList();
+    }
+}
