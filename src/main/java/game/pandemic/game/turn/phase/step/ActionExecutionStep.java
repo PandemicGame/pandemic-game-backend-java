a864257b30e88bedfb7b09de138dd6e7b89d9f0f
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-11-15 00:50:04 +0100

    Add ActionFactory to ActionExecutionStep

--- src/main/java/game/pandemic/game/turn/phase/step/ActionExecutionStep.java
+++ src/main/java/game/pandemic/game/turn/phase/step/ActionExecutionStep.java
@@ -1,6 +1,7 @@
 package game.pandemic.game.turn.phase.step;
 
 import game.pandemic.game.action.Action;
+import game.pandemic.game.action.ActionFactory;
 import game.pandemic.game.turn.phase.ActionPhase;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
@@ -9,18 +10,19 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.LinkedList;
 import java.util.List;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class ActionExecutionStep extends Step<ActionPhase> {
+    public static final ActionFactory ACTION_FACTORY = new ActionFactory();
+
     @OneToMany(cascade = CascadeType.ALL)
     private List<Action> availableActions;
 
     public ActionExecutionStep(final ActionPhase phase) {
         super(phase);
-        this.availableActions = new LinkedList<>();
+        this.availableActions = ACTION_FACTORY.createAllActions(phase.getPlayer());
     }
 }
