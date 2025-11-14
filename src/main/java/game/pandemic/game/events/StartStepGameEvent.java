125f89a4c481e982c8cb3a43594466e6b07e674f
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-11-14 19:32:36 +0100

    Add StartStepGameEvent

---
+++ src/main/java/game/pandemic/game/events/StartStepGameEvent.java
@@ -0,0 +1,26 @@
+package game.pandemic.game.events;
+
+import game.pandemic.game.Game;
+import game.pandemic.game.turn.phase.Phase;
+import game.pandemic.game.turn.phase.step.Step;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class StartStepGameEvent<P extends Phase<P>> extends GameEvent {
+    @ManyToOne
+    private P phase;
+    @OneToOne
+    private Step<P> step;
+
+    @Override
+    public void apply(final Game game) {
+        this.phase.addStep(this.step);
+    }
+}
