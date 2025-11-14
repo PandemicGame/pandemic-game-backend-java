e0ce9f4b6eb1b65d7f8772b90ed4ae744d698704
Tom <tom.weelborg@gmail.com>
2025-11-14 19:19:52 +0100

    Add ActionPhase to Turn

--- src/main/java/game/pandemic/game/turn/Turn.java
+++ src/main/java/game/pandemic/game/turn/Turn.java
@@ -4,14 +4,22 @@ import com.fasterxml.jackson.annotation.JsonIdentityReference;
 import com.fasterxml.jackson.annotation.JsonView;
 import game.pandemic.event.IEventContext;
 import game.pandemic.game.Game;
+import game.pandemic.game.action.Action;
 import game.pandemic.game.events.GameEvent;
+import game.pandemic.game.events.StartPhaseGameEvent;
 import game.pandemic.game.player.Player;
+import game.pandemic.game.turn.phase.ActionPhase;
+import game.pandemic.game.turn.phase.Phase;
 import game.pandemic.jackson.JacksonView;
 import game.pandemic.websocket.IWebSocketData;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.LinkedList;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -26,10 +34,14 @@ public class Turn implements IWebSocketData, IEventContext<Game, GameEvent> {
     @JsonView(JacksonView.Read.class)
     @JsonIdentityReference(alwaysAsId = true)
     private Player player;
+    @Setter
+    @OneToOne(cascade = CascadeType.ALL)
+    private Phase currentPhase;
 
     public Turn(final Game game) {
         this.game = game;
         this.player = this.game.getCurrentPlayer();
+        processEvent(new StartPhaseGameEvent(this, new ActionPhase(this, game.getNumberOfActionsPerTurn())));
     }
 
     @Override
@@ -45,5 +57,13 @@ public class Turn implements IWebSocketData, IEventContext<Game, GameEvent> {
     @Override
     public void restore() {
         this.game.restore();
+    }
+
+    @JsonView(JacksonView.Read.class)
+    public List<Action> getAvailableActions() {
+        if (this.currentPhase instanceof ActionPhase actionPhase) {
+            return actionPhase.getAvailableActions();
+        }
+        return new LinkedList<>();
     }
 }
