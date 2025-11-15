d941ca6d74b639bb668dc83df905d58af7f28d84
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-11-15 21:43:37 +0100

    Split Role creation in RoleFactory into separate methods

--- src/main/java/game/pandemic/game/role/RoleFactory.java
+++ src/main/java/game/pandemic/game/role/RoleFactory.java
@@ -9,11 +9,66 @@ import java.util.List;
 public class RoleFactory {
     public List<Role> createAllRoles() {
         return List.of(
-                new Role("Dispatcher", new Color(204, 0, 153), new RoleAbility(List.of(), List.of())),
-                new Role("Operations Expert", new Color(51, 204, 51), new RoleAbility(List.of(), List.of())),
-                new Role("Scientist", new Color(222, 222, 222), new RoleAbility(List.of(), List.of())),
-                new Role("Medic", new Color(255, 102, 0), new RoleAbility(List.of(), List.of())),
-                new Role("Researcher", new Color(153, 102, 51), new RoleAbility(List.of(), List.of()))
+                createDispatcher(),
+                createOperationsExpert(),
+                createScientist(),
+                createMedic(),
+                createResearcher()
+        );
+    }
+
+    private Role createDispatcher() {
+        return new Role(
+                "Dispatcher",
+                new Color(204, 0, 153),
+                new RoleAbility(
+                        List.of(),
+                        List.of()
+                )
+        );
+    }
+
+    private Role createOperationsExpert() {
+        return new Role(
+                "Operations Expert",
+                new Color(51, 204, 51),
+                new RoleAbility(
+                        List.of(),
+                        List.of()
+                )
+        );
+    }
+
+    private Role createScientist() {
+        return new Role(
+                "Scientist",
+                new Color(222, 222, 222),
+                new RoleAbility(
+                        List.of(),
+                        List.of()
+                )
+        );
+    }
+
+    private Role createMedic() {
+        return new Role(
+                "Medic",
+                new Color(255, 102, 0),
+                new RoleAbility(
+                        List.of(),
+                        List.of()
+                )
+        );
+    }
+
+    private Role createResearcher() {
+        return new Role(
+                "Researcher",
+                new Color(153, 102, 51),
+                new RoleAbility(
+                        List.of(),
+                        List.of()
+                )
         );
     }
 }
