30b6f5bcb044902903bbde1039dda0036dd1ee0b
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-11-16 18:12:01 +0100

    Override isAvailable method in ActionEffect

--- src/main/java/game/pandemic/game/action/effect/ActionEffect.java
+++ src/main/java/game/pandemic/game/action/effect/ActionEffect.java
@@ -1,7 +1,9 @@
 package game.pandemic.game.action.effect;
 
+import game.pandemic.game.Game;
 import game.pandemic.game.effect.Effect;
 import game.pandemic.game.player.Player;
+import game.pandemic.game.turn.phase.ActionPhase;
 import jakarta.persistence.Entity;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
@@ -13,5 +15,14 @@ import lombok.NoArgsConstructor;
 public abstract class ActionEffect extends Effect {
     protected ActionEffect(final Player executingPlayer, final Player targetPlayer) {
         super(executingPlayer, targetPlayer);
+    }
+
+    @Override
+    public boolean isAvailable(final Game game) {
+        return super.isAvailable(game) && game.isCurrentPlayer(this.executingPlayer) &&
+                game.getCurrentTurn().getCurrentPhase() instanceof ActionPhase actionPhase &&
+                actionPhase.getAvailableActions().stream()
+                        .flatMap(action -> action.getAvailableEffects().stream())
+                        .anyMatch(effect -> effect.equals(this));
     }
 }
