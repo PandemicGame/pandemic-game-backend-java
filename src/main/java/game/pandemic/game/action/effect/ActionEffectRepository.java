78d223b50e16b19bb80f0a75c4d712fbeb034a24
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-11-16 17:06:08 +0100

    Remove ActionEffectRepository

--- src/main/java/game/pandemic/game/action/effect/ActionEffectRepository.java
+++
@@ -1,8 +0,0 @@
-package game.pandemic.game.action.effect;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ActionEffectRepository extends JpaRepository<ActionEffect, Long> {
-}
